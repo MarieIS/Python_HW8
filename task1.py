3766173fa625caf3e1fdf2b9d4913aa583a15133
Maria <mariya-ivanova@list.ru>
2025-01-24 11:48:26 +0300

    the solution of the first task was updated

--- task1.py
+++ task1.py
@@ -1,26 +1,19 @@
 def get_sum(my_file_name):
     numbers_list = list()
     tot_sum = 0
-    f = open(my_file_name, 'r', encoding='utf-8')
-    rows_list = f.readlines() # список строк в текстовом режиме
-    for row in rows_list:
-        numbers_list.append(row.split())
-    
-    print(numbers_list)
+    with open(my_file_name, 'r', encoding='utf-8') as f:
+        rows_list = f.readlines() # список строк в текстовом режиме
+        for row in rows_list:
+            numbers_list.append(*row.split())
         
-    for number in numbers_list:
-        print(number)
-        tot_sum += int(*number)
-    
-    f.close()
+        for number in numbers_list:
+            tot_sum += int(number)
     
     return tot_sum
 
 def write_sum(my_file_name, value):
-    f = open(my_file_name, 'w', encoding='utf-8')
-    f.write(value)
-    f.close()
+    with open(my_file_name, 'w', encoding='utf-8') as f:
+        f.write(str(value))
 
 my_sum = get_sum('numbers.txt')
-print(my_sum)
-write_sum('answers.txt', str(my_sum))
+write_sum('answers.txt', my_sum)

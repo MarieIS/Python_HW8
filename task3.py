c3d5ba8ac378aafe30acfb6ef6d631e7cb496e22
Maria <mariya-ivanova@list.ru>
2025-01-25 19:42:26 +0300

    the solution of the third task was improved

--- task3.py
+++ task3.py
@@ -1,25 +1,18 @@
-# читаем файл
-# строки расщепляем по пробелу на три части занося в список
-# дальше этот список анализируем по баллам
-# результат записываем во второй файл
-
 def read_list(my_file_name):
     res_list = list()
     with open(my_file_name, 'r', encoding='utf-8') as f:
         rows_list = list()
         rows_list = f.readlines()
-        list_count = int(*rows_list[0].split()) # заданное количество баллов
+        list_count = int(*rows_list[0].split())
         for row in range(1, len(rows_list)):
             info_list = list()
             info_list = rows_list[row].split()
             if int(info_list[2]) > list_count:
                 res_list.append(info_list)
-    #print(res_list)
     return res_list
 
 def write_list(my_file_name, input_list):
     input_list.sort(key=lambda i: i[2], reverse=True)
-    print(input_list)
     with open(my_file_name, 'w', encoding='utf-8') as f:
         for row in range(len(input_list)):
             string = str(row+1) + ') ' + input_list[row][1][0] + '. ' + input_list[row][0] + ' ' + str(input_list[row][2]) + '\n'
